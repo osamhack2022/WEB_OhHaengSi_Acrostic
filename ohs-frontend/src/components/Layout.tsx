04918bc1a01f9fac6ced535f412a6a274b6573ae
kimseongyu <kskksk0925@naver.com>
2022-09-26 15:31:32 +0000

    Update Layout

---
+++ ohs-frontend/src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Nav, { NavLink } from 'react-router-dom';
+import Contents from './Contents';
+
+function Layout(): React.ReactElement {
+  const [content, setContent] = React.useState(1);
+
+  return (
+    <div>
+      <img src="" alt="logo" onClick={() => setContent(1)} />
+      <div>
+        <ul>
+          <li onClick={() => setContent(1)}>인원현황</li>
+          <li onClick={() => setContent(2)}>근무표</li>
+          <li onClick={() => setContent(3)}>임무분담제</li>
+          <li onClick={() => setContent(4)}>전파사항</li>
+          <li onClick={() => setContent(5)}>긴급</li>
+        </ul>
+      </div>
+      <Contents content={content} />
+    </div>
+  );
+}
+
+export default Layout;
