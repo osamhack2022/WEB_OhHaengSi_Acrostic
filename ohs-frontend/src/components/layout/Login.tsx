e8d742e886edaf1d289ffa50f40d783b74ba611c
kimseongyu <kskksk0925@naver.com>
2022-10-02 14:40:31 +0000

    Change folder

--- ohs-frontend/src/components/layout/Login.tsx
+++ ohs-frontend/src/components/layout/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { setCookie } from '../utils/Cookie';
+import { setCookie } from '../../utils/Cookie';
 import { useForm } from 'react-hook-form';
 
 function Login(): React.ReactElement {
