2ac5a8b861019e40dcd3d0dafdba0e745d9dce5b
kimsuyoung <suyoung154@gmail.com>
2022-10-03 04:48:06 +0000

    Added dummy data about room information

--- ohs-backend/src/room/room.service.ts
+++ ohs-backend/src/room/room.service.ts
@@ -20,7 +20,41 @@ export class RoomService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} room`;
+    return {
+      room: {
+        id
+      },
+      members: [
+        {
+          name: '김병장',
+          rank: 4,
+          rank_name: '병장',
+          status: '휴가',
+        },
+        {
+          name: '박일병',
+          rank: 2,
+          rank_name: '일병',
+          status: '열중',
+        },
+        {
+          name: '정상병',
+          rank: 3,
+          rank_name: '상병',
+          status: '기타',
+        },
+      ],
+      summary: {
+        total: 8,
+        absence: 3,
+        current: 5,
+        absence_reasons: [
+          ['휴가', 1],
+          ['기타', 1],
+          ['근무', 1],
+        ],
+      },
+    };
   }
 
   update(id: number, updateRoomDto: UpdateRoomDto) {
