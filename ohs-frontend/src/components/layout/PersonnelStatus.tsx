39f08cda09642bdf7ba01c6fdd8b252d0933ec84
kimseongyu <kskksk0925@naver.com>
2022-10-05 11:52:12 +0000

    Update PersoonelStatus

--- ohs-frontend/src/components/layout/PersonnelStatus.tsx
+++ ohs-frontend/src/components/layout/PersonnelStatus.tsx
@@ -1,42 +1,70 @@
 import React from 'react';
+import Select from 'react-select';
+import styles from '../../styles/PersonnelStatus.module.scss';
 import UsePersonnelData from '../../hooks/UsePersonnelData';
+import UsePersonnelStatusSelect from '../../hooks/UsePersonnelStatusSelect';
 
 function PersonnelStatus(): React.ReactElement {
-  const { personnelData, currentPersonnelData, totalPersonnel, currentPersonnel } = UsePersonnelData();
+  const { room, members, summary } = UsePersonnelData();
+  const { options, defaultValue, selectStyle } = UsePersonnelStatusSelect();
+
+  const dormitory = () => {
+    return (
+      <div className={styles.dormitory}>
+        <h2>{room.id} 생활관</h2>
+        {members.map((element, idx) => {
+          return (
+            <div key={idx} className={styles.person}>
+              <label>{element.rank_name} </label>
+              <label>{element.name} </label>
+              <div className={styles.selectBox}>
+                {/* styles={selectStyle} 로 select styling하기*/}
+                <Select defaultValue={defaultValue(element.status)} options={options} />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  const status = () => {
+    return (
+      <label className={styles.status}>
+        총원 {summary?.total} / 현재원 {summary?.current} / 열외 {summary?.absence}
+      </label>
+    );
+  };
+
+  const absenceStatus = () => {
+    return (
+      <div className={styles.absence}>
+        <label>열외사유</label>
+        <table>
+          <thead>
+            <tr>
+              {summary?.absence_reasons.map((element, idx) => {
+                return <th key={idx}>{element[0]}</th>;
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {summary?.absence_reasons.map((element, idx) => {
+                return <td key={idx}>{element[1]}</td>;
+              })}
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+
   return (
-    <div>
-      {personnelData.map((element, idx) => {
-        return (
-          <div key={idx}>
-            <label>{element.class} </label>
-            <label>{element.name} </label>
-            <select id="state" defaultValue={element.state}>
-              <option value="근무">근무</option>
-              <option value="휴가">휴가</option>
-            </select>
-          </div>
-        );
-      })}
-      <p>
-        총원 : {totalPersonnel}, 현재원 : {currentPersonnel}
-      </p>
-      <p>열외사유</p>
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(currentPersonnelData).map((element, idx) => {
-              return <td key={idx}>{element}</td>;
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {Object.keys(currentPersonnelData).map((element, idx) => {
-              return <td key={idx}>{currentPersonnelData[element]}</td>;
-            })}
-          </tr>
-        </tbody>
-      </table>
+    <div className={styles.personnelStatus}>
+      {dormitory()}
+      {status()}
+      {absenceStatus()}
     </div>
   );
 }
--- ohs-frontend/src/hooks/UsePersonnelData.tsx
+++ ohs-frontend/src/hooks/UsePersonnelData.tsx
@@ -1,45 +1,103 @@
-import React, { useState } from 'react';
-import { strNumDic } from '../utils/DictionaryType';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-type personnelDataType = { class: string; name: string; state: string };
+type room = { id: number };
+type members = { name: string; rank: number; rank_name: string; status: string };
+type summary = { total: number; absence: number; current: number; absence_reasons: (string | number)[][] };
+type data = { room: room; members: members[]; summary: summary };
+
+const example_data: data = {
+  room: {
+    id: 2,
+  },
+  members: [
+    {
+      name: '김병장',
+      rank: 4,
+      rank_name: '병장',
+      status: '휴가',
+    },
+    {
+      name: '박일병',
+      rank: 2,
+      rank_name: '일병',
+      status: '근무',
+    },
+    {
+      name: '정상병',
+      rank: 3,
+      rank_name: '상병',
+      status: '기타',
+    },
+    {
+      name: '이이병',
+      rank: 1,
+      rank_name: '이병',
+      status: '기타',
+    },
+    {
+      name: '이이병',
+      rank: 1,
+      rank_name: '이병',
+      status: '기타',
+    },
+    {
+      name: '이이병',
+      rank: 1,
+      rank_name: '이병',
+      status: '기타',
+    },
+    {
+      name: '이이병',
+      rank: 1,
+      rank_name: '이병',
+      status: '기타',
+    },
+    {
+      name: '이이병',
+      rank: 1,
+      rank_name: '이병',
+      status: '기타',
+    },
+  ],
+  summary: {
+    total: 8,
+    absence: 3,
+    current: 5,
+    absence_reasons: [
+      ['근무', 1],
+      ['외출', 1],
+      ['외박', 1],
+      ['휴가', 1],
+      ['외진', 1],
+      ['기타', 1],
+    ],
+  },
+};
 
 function UsePersonnelData() {
   // 인원현황을 저장하는 오브젝트 배열
-  const [personnelData, setPersonnelData] = useState<personnelDataType[]>([
-    { class: '상병', name: '김선규', state: '근무' },
-    { class: '상병', name: '김선규', state: '휴가' },
-    { class: '일병', name: '김수영', state: '근무' },
-    { class: '일병', name: '김수영', state: '휴가' },
-  ]);
-  // 현재원의 상태를 {종류:인원}으로 저장하는 오브젝트
-  const [currentPersonnelData, setCurrentPersonnelData] = useState<strNumDic>(() => {
-    const currentData: strNumDic = {};
-    personnelData.map((element, idx) => {
-      if (currentData[element.state]) {
-        currentData[element.state] += 1;
-      } else {
-        currentData[element.state] = 1;
-      }
+  const [room, setRoom] = useState<room>({ id: 0 });
+  const [members, setMembersData] = useState<members[]>([]);
+  const [summary, setSummary] = useState<summary>();
+
+  const getData = async () => {
+    axios.get('https://ohs.run.goorm.io/room').then((response: any) => {
+      console.log(response);
     });
-    return currentData;
-  });
-  // 총원을 저장하는 변수
-  const [totalPersonnel, setTotalPersonnel] = useState(personnelData.length);
-  // 현재원을 저장하는 변수
-  const [currentPersonnel, setCurrentPersonnel] = useState(() => {
-    let count = 0;
-    personnelData.map(element => {
-      if (element.state !== '근무') {
-        count += 1;
-      }
-    });
-    return count;
-  });
+    setRoom(example_data.room);
+    setMembersData(example_data.members);
+    setSummary(example_data.summary);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return {
-    personnelData: personnelData,
-    currentPersonnelData: currentPersonnelData,
-    totalPersonnel: totalPersonnel,
-    currentPersonnel: currentPersonnel,
+    room: room,
+    members: members,
+    summary: summary,
   };
 }
 
