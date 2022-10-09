b6cd6fa58fa31cf7a7132510fd19bf2fd0ebe8ad
kimseongyu <kskksk0925@naver.com>
2022-10-09 14:55:43 +0000

    Update Login

--- ohs-frontend/src/components/layout/Login.tsx
+++ ohs-frontend/src/components/layout/Login.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styles from '../../styles/Login.module.scss';
 import { setCookie } from '../../utils/Cookie';
 import { useForm } from 'react-hook-form';
 
@@ -12,10 +13,16 @@ function Login(): React.ReactElement {
   };
 
   return (
-    <div>
+    <div className={styles.login}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text" {...register('id')} />
-        <input type="password" {...register('pw')} />
+        <div>
+          <label>ID</label>
+          <input type="text" {...register('id')} />
+        </div>
+        <div>
+          <label>PW</label>
+          <input type="password" {...register('pw')} />
+        </div>
         <input type="submit" />
       </form>
     </div>
