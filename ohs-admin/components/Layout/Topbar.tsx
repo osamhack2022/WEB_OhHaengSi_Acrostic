b7b11e9a82829636c82f3477391ff54ead0a40ad
kimsuyoung <suyoung154@gmail.com>
2022-10-10 13:46:58 +0000

    Remove useless code in topbar

--- ohs-admin/components/Layout/Topbar.tsx
+++ ohs-admin/components/Layout/Topbar.tsx
@@ -4,37 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Topbar() {
   return (
     <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-      {/* Sidebar Toggle (Topbar) */}
       <button
         id="sidebarToggleTop"
         className="btn btn-link d-md-none rounded-circle mr-3"
       >
         <FontAwesomeIcon icon={solid("bars")} className="fa fa-bars" />
       </button>
-      {/* Topbar Search */}
-      <form className="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div className="input-group">
-          <input
-            type="text"
-            className="form-control bg-light border-0 small"
-            placeholder="Search for..."
-            aria-label="Search"
-            aria-describedby="basic-addon2"
-          />
-          <div className="input-group-append">
-            <button className="btn btn-primary" type="button">
-              <FontAwesomeIcon
-                icon={solid("search")}
-                size="sm"
-                className="fas fa-search fa-sm"
-              />
-            </button>
-          </div>
-        </div>
-      </form>
-      {/* Topbar Navbar */}
       <ul className="navbar-nav ml-auto">
-        {/* Nav Item - Search Dropdown (Visible Only XS) */}
         <li className="nav-item dropdown no-arrow d-sm-none">
           <a
             className="nav-link dropdown-toggle"
@@ -51,7 +27,6 @@ export default function Topbar() {
               className="fas fa-search fa-fw"
             />
           </a>
-          {/* Dropdown - Messages */}
           <div
             className="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
             aria-labelledby="searchDropdown"
@@ -78,7 +53,6 @@ export default function Topbar() {
             </form>
           </div>
         </li>
-        {/* Nav Item - Alerts */}
         <li className="nav-item dropdown no-arrow mx-1">
           <a
             className="nav-link dropdown-toggle"
@@ -93,10 +67,8 @@ export default function Topbar() {
               icon={solid("bell")}
               className="fas fa-bell fa-fw"
             />
-            {/* Counter - Alerts */}
             <span className="badge badge-danger badge-counter">3+</span>
           </a>
-          {/* Dropdown - Alerts */}
           <div
             className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="alertsDropdown"
@@ -155,7 +127,6 @@ export default function Topbar() {
             </a>
           </div>
         </li>
-        {/* Nav Item - Messages */}
         <li className="nav-item dropdown no-arrow mx-1">
           <a
             className="nav-link dropdown-toggle"
@@ -171,10 +142,8 @@ export default function Topbar() {
               fixedWidth
               className="fas fa-envelope fa-fw"
             />
-            {/* Counter - Messages */}
             <span className="badge badge-danger badge-counter">7</span>
           </a>
-          {/* Dropdown - Messages */}
           <div
             className="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="messagesDropdown"
@@ -257,7 +226,6 @@ export default function Topbar() {
           </div>
         </li>
         <div className="topbar-divider d-none d-sm-block" />
-        {/* Nav Item - User Information */}
         <li className="nav-item dropdown no-arrow">
           <a
             className="nav-link dropdown-toggle"
@@ -272,7 +240,6 @@ export default function Topbar() {
               Douglas McGee
             </span>
           </a>
-          {/* Dropdown - User Information */}
           <div
             className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
             aria-labelledby="userDropdown"
