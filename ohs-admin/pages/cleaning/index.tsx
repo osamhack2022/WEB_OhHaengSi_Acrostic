209fd07ac2f90e89d915070c6de6ad4cdb039d65
kimsuyoung <suyoung154@gmail.com>
2022-10-10 13:42:56 +0000

    Added dummy pages

---
+++ ohs-admin/pages/cleaning/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Layout from "../../components/Layout/Layout";
+
+const CleaningPage: NextPage = () => {
+  return <Layout>CleaningPage</Layout>;
+};
+
+export default CleaningPage;
---
+++ ohs-admin/pages/soldier/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import Layout from "../../components/Layout/Layout";
+
+const SoldierPage: NextPage = () => {
+  return <Layout>SoldierPage</Layout>;
+};
+
+export default SoldierPage;
