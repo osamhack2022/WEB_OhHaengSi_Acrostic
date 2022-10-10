94d46c27af33e31dcea85f37b0f4d0254494dab7
kimsuyoung <suyoung154@gmail.com>
2022-10-10 12:59:28 +0000

    Update site title

--- ohs-admin/pages/_document.tsx
+++ ohs-admin/pages/_document.tsx
@@ -4,10 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-          rel="stylesheet"
-        />
+        <title>오행시 관리자</title>
       </Head>
       <body>
         <Main />
