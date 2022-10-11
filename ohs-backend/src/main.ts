0ca61fddf19717d74212b3cbb6803580ecebd6ec
kimsuyoung <suyoung154@gmail.com>
2022-10-11 03:22:59 +0000

    update some app settings

--- ohs-backend/src/main.ts
+++ ohs-backend/src/main.ts
@@ -1,10 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
+import { json } from 'express';
+
+morgan.token('body', (req) => {
+  return JSON.stringify(req['body']);
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use(json());
+  app.use(
+    morgan(':method :url :body', {
+      immediate: true,
+    }),
+  );
+
   await app.listen(4000);
 }
 bootstrap();
