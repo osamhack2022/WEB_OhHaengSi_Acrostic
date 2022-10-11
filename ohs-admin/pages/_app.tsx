542c0da21db4d395e788fb1b082b3b4e33b8e80a
kimsuyoung <suyoung154@gmail.com>
2022-10-11 08:02:20 +0000

    Move title from _document to _app

--- ohs-admin/pages/_app.tsx
+++ ohs-admin/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>오행시 관리자</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- ohs-admin/pages/_document.tsx
+++ ohs-admin/pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <title>오행시 관리자</title>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
