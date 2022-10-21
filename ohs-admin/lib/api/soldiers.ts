02af8117dd8715ed1361b6ce5188d8778d7fde9f
kimsuyoung <suyoung154@gmail.com>
2022-10-21 05:26:44 +0000

    병사 목록 출력 모듈화

--- ohs-admin/lib/api/soldiers.ts
+++ ohs-admin/lib/api/soldiers.ts
@@ -1,3 +1,15 @@
-import { post } from "./common";
+import { get, post } from "./common";
+
+export type Soldier = {
+  id: number;
+  name: string;
+  rank: number; // 1: 이병, 2: 일병, 3: 상병, 4: 병장
+  status: string;
+  roomId: number;
+};
 
 export const createSoldier = (data: any) => post("/soldiers", data);
+
+export const getSoldiers = () => get("/soldiers");
+
+export const getSoldier = (id: number) => get(`/soldiers/${id}`);
