818648feb39822336d7a0cef9de4947bc34e7eac
kimsuyoung <suyoung154@gmail.com>
2022-10-21 00:55:06 +0000

    기본 API 요청 기능 구현

--- ohs-admin/lib/api/common.ts
+++ ohs-admin/lib/api/common.ts
@@ -14,34 +14,79 @@ export function joinUrl(...paths: string[]) {
   );
 }
 
-export async function get(url: string) {
+export const DEFAULT_OPTIONS: RequestInit = {
+  mode: "cors", // no-cors, *cors, same-origin
+  cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+  credentials: "same-origin", // include, *same-origin, omit
+  headers: {
+    "Content-Type": "application/json",
+  },
+  redirect: "follow", // manual, *follow, error
+  referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+};
+
+function createOptions(options?: RequestInit, accessToken?: string) {
+  const opts = options ?? DEFAULT_OPTIONS;
+
+  if (accessToken) {
+    opts.headers = { ...opts.headers, Bearer: accessToken };
+  }
+
+  return opts;
+}
+
+export async function get(
+  url: string,
+  options?: RequestInit,
+  accessToken?: string
+) {
   const response = await fetch(joinUrl(baseUrl, url), {
-    method: "POST", // *GET, POST, PUT, DELETE 등
-    mode: "cors", // no-cors, *cors, same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow", // manual, *follow, error
-    referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    ...createOptions(options, accessToken),
+    method: "GET", // *GET, POST, PUT, DELETE 등
   });
 
   return response.json(); // JSON 응답을 네이티브 JavaScript 객체로 파싱
 }
 
-export async function post<T>(url: string, data: any) {
+export async function post<T>(
+  url: string,
+  data: any,
+  options?: RequestInit,
+  accessToken?: string
+) {
   const response = await fetch(joinUrl(baseUrl, url), {
+    ...createOptions(options, accessToken),
     method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
+    body: JSON.stringify(data),
+  });
+
+  return response.json() as T;
+}
+
+export async function patch<T>(
+  url: string,
+  data: any,
+  options?: RequestInit,
+  accessToken?: string
+) {
+  const response = await fetch(joinUrl(baseUrl, url), {
+    ...createOptions(options, accessToken),
+    method: "PATCH",
+    body: JSON.stringify(data),
+  });
+
+  return response.json() as T;
+}
+
+export async function del<T>(
+  url: string,
+  data: any,
+  options?: RequestInit,
+  accessToken?: string
+) {
+  const response = await fetch(joinUrl(baseUrl, url), {
+    ...createOptions(options, accessToken),
+    method: "DELETE",
     body: JSON.stringify(data),
   });
 
