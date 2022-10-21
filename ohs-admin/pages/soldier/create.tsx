fd19c99d98009b62b968c25bba316e150bb49c29
kimsuyoung <suyoung154@gmail.com>
2022-10-21 01:38:35 +0000

    폼 이벤트 처리

--- ohs-admin/pages/soldier/create.tsx
+++ ohs-admin/pages/soldier/create.tsx
@@ -1,34 +1,44 @@
+import { useForm } from "react-hook-form";
 import ContentCard from "../../components/common/card/ContentCard";
-import Table from "../../components/common/Table";
 import Layout from "../../components/Layout/Layout";
 
+type FormData = {
+  name: string;
+  rank: string;
+  roomId: number;
+  status: string;
+};
+
 const SoldierCreatePage = () => {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = handleSubmit((data) => console.log(data));
+  // firstName and lastName will have correct type
+
   return (
     <Layout>
       <div className="row justify-content-center">
         <ContentCard className="col-xl-6 col-md-8" title="병사 추가">
-          <form>
+          <form onSubmit={onSubmit}>
             <div className="form-group ">
               <label className="form-label">이름</label>
-              <input
-                type="text"
-                className="form-control "
-                id="exampleFirstName"
-                placeholder="이름"
-              />
+              <input className="form-control " {...register("name")} required />
             </div>
             <div className="form-group ">
               <label className="form-label">계급</label>
-              <input
-                type="text"
-                className="form-control "
-                id="exampleFirstName"
-                placeholder="계급"
-              />
+              <input className="form-control " {...register("rank")} required />
             </div>
             <div className="form-group">
               <label className="form-label">상태</label>
-              <select className="form-control ">
+              <select
+                className="form-control "
+                {...register("status")}
+                required
+              >
                 <option>열중</option>
                 <option>근무</option>
                 <option>휴가</option>
@@ -37,14 +47,18 @@ const SoldierCreatePage = () => {
             </div>
             <div className="form-group">
               <label className="form-label">생활관</label>
-              <select className="form-control ">
+              <select
+                className="form-control "
+                {...register("roomId")}
+                required
+              >
                 <option>1</option>
                 <option>2</option>
                 <option>3</option>
                 <option>4</option>
               </select>
             </div>
-            <button type="button" className="btn btn-primary btn-block btn-lg">
+            <button type="submit" className="btn btn-primary btn-block btn-lg">
               병사 추가
             </button>
             <button type="button" className="btn btn-danger btn-block btn-lg">
