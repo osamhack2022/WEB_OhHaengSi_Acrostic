e290fbb313c066efb6d44eb913e480206130a02b
kimsuyoung <suyoung154@gmail.com>
2022-10-28 01:10:57 +0000

    차트 기능 및 샘플 추가

--- ohs-admin/pages/index.tsx
+++ ohs-admin/pages/index.tsx
@@ -7,9 +7,39 @@ import PageHeader from "../components/common/PageHeader";
 import Container from "../components/Layout/Container";
 import Layout from "../components/Layout/Layout";
 import Row from "../components/Layout/Row";
-import withCredential from "../lib/auth/withCredential";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+} from "chart.js";
+import { Bar, Pie } from "react-chartjs-2";
+import { randomInt } from "crypto";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement
+);
 
 const Home: NextPage = () => {
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
   return (
     <Layout>
       <Container fluid>
@@ -63,38 +93,84 @@ const Home: NextPage = () => {
           </SummaryCard>
         </Row>
         <Row>
-          <ContentCard title="Earnings Overview" className="col-xl-8 col-lg-7">
-            <div className="chart-area">
-              <canvas id="myAreaChart" />
-            </div>
+          <ContentCard title="근무 공정표(1)" className="col-6">
+            <Bar
+              data={{
+                labels,
+                datasets: [
+                  {
+                    label: "Dataset 1",
+                    data: labels.map(() => Math.floor(Math.random() * 1000)),
+                    backgroundColor: "rgba(255, 99, 132, 0.5)",
+                  },
+                  {
+                    label: "Dataset 2",
+                    data: labels.map(() => Math.floor(Math.random() * 1000)),
+                    backgroundColor: "rgba(53, 162, 235, 0.5)",
+                  },
+                ],
+              }}
+            />
           </ContentCard>
-          <ContentCard title="Earnings Overview" className="col-xl-4 col-lg-5">
-            <div className="chart-pie pt-4 pb-2">
-              <canvas id="myPieChart" />
-            </div>
-            <div className="mt-4 text-center small">
-              <span className="mr-2">
-                <FontAwesomeIcon
-                  icon={regular("laugh-wink")}
-                  className="fas fa-circle text-primary"
-                />{" "}
-                Direct
-              </span>
-              <span className="mr-2">
-                <FontAwesomeIcon
-                  icon={regular("laugh-wink")}
-                  className="fas fa-circle text-success"
-                />{" "}
-                Social
-              </span>
-              <span className="mr-2">
-                <FontAwesomeIcon
-                  icon={regular("laugh-wink")}
-                  className="fas fa-circle text-info"
-                />{" "}
-                Referral
-              </span>
-            </div>
+          <ContentCard title="근무 공정표(2)" className="col-3">
+            <Pie
+              data={{
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                  {
+                    label: "# of Votes",
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                      "rgba(255, 206, 86, 0.2)",
+                      "rgba(75, 192, 192, 0.2)",
+                      "rgba(153, 102, 255, 0.2)",
+                      "rgba(255, 159, 64, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                      "rgba(255, 206, 86, 1)",
+                      "rgba(75, 192, 192, 1)",
+                      "rgba(153, 102, 255, 1)",
+                      "rgba(255, 159, 64, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+          </ContentCard>
+          <ContentCard title="근무 공정표(2)" className="col-3">
+            <Pie
+              data={{
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [
+                  {
+                    label: "# of Votes",
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                      "rgba(255, 99, 132, 0.2)",
+                      "rgba(54, 162, 235, 0.2)",
+                      "rgba(255, 206, 86, 0.2)",
+                      "rgba(75, 192, 192, 0.2)",
+                      "rgba(153, 102, 255, 0.2)",
+                      "rgba(255, 159, 64, 0.2)",
+                    ],
+                    borderColor: [
+                      "rgba(255, 99, 132, 1)",
+                      "rgba(54, 162, 235, 1)",
+                      "rgba(255, 206, 86, 1)",
+                      "rgba(75, 192, 192, 1)",
+                      "rgba(153, 102, 255, 1)",
+                      "rgba(255, 159, 64, 1)",
+                    ],
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
           </ContentCard>
         </Row>
       </Container>
